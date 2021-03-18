5eb3040e27cb826d0509b29db1b959f68bacc2e6
yusyun <y2000manen@docomo.ne.jp>
2021-03-18 19:02:10 +0900

    update

--- employproject/settings.py
+++ employproject/settings.py
@@ -146,13 +146,10 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']
-
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(locals()) 
+    django_heroku.settings(locals() ,staticfiles=False) 
