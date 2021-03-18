cd4895ffff2830ac5751927ce9f4509c605d794a
yusyun <y2000manen@docomo.ne.jp>
2021-03-19 00:03:10 +0900

    cew

--- employproject/settings.py
+++ employproject/settings.py
@@ -150,6 +150,6 @@ db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 if not DEBUG:
-    SECRET_KEY =os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals() ,staticfiles=False) 
