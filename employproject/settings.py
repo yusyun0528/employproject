7420ab6461f01e6707400d96f252e0f874316175
yusyun <y2000manen@docomo.ne.jp>
2021-03-18 13:57:13 +0900

    new

--- employproject/settings.py
+++ employproject/settings.py
@@ -147,7 +147,7 @@ except ImportError:
     pass
 
 if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']))
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
