d8134bbdded353cf66816ed05b3479611a8de9e3
yusyun <y2000manen@docomo.ne.jp>
2021-03-25 23:38:44 +0900

    a

--- employproject/settings.py
+++ employproject/settings.py
@@ -147,11 +147,29 @@ RQ_QUEUES = {
         'HOST': 'localhost',
         'PORT': 6379,
         'DB': 0,
-        'PASSWORD': '',
+        'PASSWORD': 'some-password',
         'DEFAULT_TIMEOUT': 360,
     },
+    'with-sentinel': {
+        'SENTINELS': [('localhost', 26736), ('localhost', 26737)],
+        'MASTER_NAME': 'redismaster',
+        'DB': 0,
+        'PASSWORD': 'secret',
+        'SOCKET_TIMEOUT': None,
+        'CONNECTION_KWARGS': {
+            'socket_connect_timeout': 0.3
+        },
+    },
+    'high': {
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'DEFAULT_TIMEOUT': 500,
+    },
+    'low': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+    }
 }
-RQ_API_TOKEN = 'hoge'
 
 try:
     from .local_settings import *
--- employproject/urls.py
+++ employproject/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employapp.urls')),
+    path('django-rq/', include('django_rq.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
