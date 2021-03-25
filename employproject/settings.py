64fb4d66427054c4c61be6b257c6b19fcfd94c7e
yusyun <y2000manen@docomo.ne.jp>
2021-03-26 00:37:01 +0900

    a

--- employproject/settings.py
+++ employproject/settings.py
@@ -161,9 +161,8 @@ RQ_QUEUES = {
         'DEFAULT_TIMEOUT': 500,
     },
     'low': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'DEFAULT_TIMEOUT': 500,
     }
 }
 
